c6ea20c9eb6a26ef0af27a89af2f300f6aeaae2a
Sohaib <m.sohaib20631@gmail.com>
2026-05-07 16:30:24 +0500

    changes

--- Dryfruitwebsite/vite.config.js
+++ Dryfruitwebsite/vite.config.js
@@ -10,18 +10,6 @@ export default defineConfig({
   build: {
     outDir: 'dist',
     sourcemap: false,
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            if (id.includes('react') || id.includes('react-dom') || id.includes('react-router-dom')) {
-              return 'vendor-react';
-            }
-            return 'vendor';
-          }
-        },
-      },
-    },
   },
   server: {
     port: 3000,
